0d34cb050d8366c267d65a3bb80fdc7c3c7d9b04
Ross Farmer <rzf0030@auburn.edu>
2023-01-30 22:36:37 -0600

    Initial Commit

---
+++ Assignment3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment3
+//
+//  Created by Ross Farmer on 1/30/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
