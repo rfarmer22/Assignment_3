06683b00a6185f398c4145cbd7a3bea1629d927d
Ross Farmer <rzf0030@auburn.edu>
2023-02-03 13:57:27 -0600

    Added time to label and background changes based on system time

--- Assignment3/ViewController.swift
+++ Assignment3/ViewController.swift
@@ -9,12 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var clock: UILabel!
+    @IBOutlet weak var test: UILabel!
+    @IBOutlet weak var background: UIImageView!
+    
+    var timer = Timer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        }
-
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector:#selector(self.currentTime) , userInfo: nil, repeats: true)
+        setBackground()
     }
+
+    @objc func currentTime() {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "E, d MMM yyyy HH:mm:ss"
+        clock.text = formatter.string(from: Date())
+        }
+    
+    @objc func setBackground(){
+        let hour = Calendar.current.component(.hour, from: Date())
+        
+        if(hour > 11){
+            background.image = UIImage(named:"PM")
+            background.contentMode = UIView.ContentMode.scaleAspectFill
+        } else {
+            background.image = UIImage(named:"AM")
+            background.contentMode = UIView.ContentMode.scaleAspectFill
+        }
+    }
+}
 
 
