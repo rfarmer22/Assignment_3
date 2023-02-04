a32e51e0a5b9a905de1b03c93647a1a08252c4de
Ross Farmer <rzf0030@auburn.edu>
2023-02-04 10:05:05 -0600

    More updates

--- Assignment3/ViewController.swift
+++ Assignment3/ViewController.swift
@@ -12,13 +12,22 @@ class ViewController: UIViewController {
     @IBOutlet weak var clock: UILabel!
     @IBOutlet weak var test: UILabel!
     @IBOutlet weak var background: UIImageView!
+    @IBOutlet weak var startAndStop: UIButton!
     
     var timer = Timer()
+    var count = 1
+    var timeLeft : Int?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector:#selector(self.currentTime) , userInfo: nil, repeats: true)
+        
         setBackground()
+        
+        startAndStop.layer.cornerRadius = 12
+        startAndStop.layer.borderWidth = 2
+        startAndStop.layer.borderColor = UIColor.black.cgColor
+        startAndStop.layer.backgroundColor = UIColor.gray.cgColor
     }
 
     @objc func currentTime() {
@@ -26,7 +35,7 @@ class ViewController: UIViewController {
         formatter.dateFormat = "E, d MMM yyyy HH:mm:ss"
         clock.text = formatter.string(from: Date())
         }
-    
+
     @objc func setBackground(){
         let hour = Calendar.current.component(.hour, from: Date())
         
@@ -38,6 +47,19 @@ class ViewController: UIViewController {
             background.contentMode = UIView.ContentMode.scaleAspectFill
         }
     }
+    
+    @IBAction func startAndStopAction(_ sender: Any) {
+        if(count % 2 == 0) {
+            startAndStop.setTitle("Start Music", for: .normal)
+            count += 1
+        } else {
+            startAndStop.setTitle("Stop Music", for: .normal)
+            count += 1
+        }
+        
+    }
+    
+
 }
 
 
